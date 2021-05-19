d7b0443bc96b7de511d1c9b2786b856067c56292
dung111999 <52844170+dung111999@users.noreply.github.com>
2021-05-19 13:55:43 +0700

    Update Router.js

--- src/Router.js
+++ src/Router.js
@@ -6,8 +6,9 @@ import { createAppContainer } from "react-navigation";
 import MainPage from './component/MainPage';
 import List from './component/List';
 import Info from './component/Info';
+import Favorites from './component/Favourite'
 
-export const ListStack = createStackNavigator({
+export const ListStack_1 = createStackNavigator({
     MainPage: {
         screen: MainPage,
     },
@@ -19,20 +20,41 @@ export const ListStack = createStackNavigator({
         headerMode: 'none',
     });
 
+
+export const ListStack_2 = createStackNavigator({
+    Favourites: {
+        screen: Favorites,
+    },
+    Info: {
+        screen: Info,
+    }
+},
+    {
+        headerMode: 'none',
+    });
+
 export const Router = createAppContainer(createBottomTabNavigator({
     Map: {
-        screen: ListStack,
+        screen: ListStack_1,
         navigationOptions: {
             tabBarIcon: ({ tintColor }) => (
                 <Icon name='public' color={tintColor} size={25} />
             )
-        }
+        },
     },
     List: {
         screen: List,
         navigationOptions: {
             tabBarIcon: ({ tintColor }) => (
                 <Icon name='view-list' color={tintColor} size={25} />
+            )
+        }
+    },
+    Favorites: {
+        screen: ListStack_2,
+        navigationOptions: {
+            tabBarIcon: ({ tintColor }) => (
+                <Icon name='heart' color={tintColor} size={25} />
             )
         }
     },
